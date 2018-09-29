2832eb72f1510df2d7d13143d3708f99976d3c54
cs <csleleljl@gmail.com>
2018-09-29 22:04:04 +0800

    add mock data to show person list

--- app/src/main/java/com/huawei/bloomfate/ui/MyPersonRecyclerViewAdapter.java
+++ app/src/main/java/com/huawei/bloomfate/ui/MyPersonRecyclerViewAdapter.java
@@ -37,8 +37,8 @@ public class MyPersonRecyclerViewAdapter extends RecyclerView.Adapter<MyPersonRe
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).id);
-        holder.mContentView.setText(mValues.get(position).content);
+//        holder.mIdView.setText(mValues.get(position).id);
+//        holder.mContentView.setText(mValues.get(position).content);
 
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
